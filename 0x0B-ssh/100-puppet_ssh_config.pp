c7230f9f84664b0320fa168a44b1fb8c6cf93fbb
Edwardropde <edwardropha@gmail.com>
2023-11-26 10:57:21 +0000

    corrections

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,15 @@
 # 100-puppet_ssh_config.pp
+include stdlib
 
 file_line { 'Turn off passwd auth':
+  ensure => present,
   path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^#?PasswordAuthentication',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
 file_line { 'Declare identity file':
   path   => '/home/vagrant/.ssh/config',
-  line   => 'IdentityFile /home/vagrant/.ssh/school',
-  match  => '^#?IdentityFile',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
